57f7c98a17f1060884ecbea4a46acca5ad7159f6
Ancor Cruz <ancor.cruz.ext@qonto.com>
2023-05-09 00:47:40 +0100

    Delete dependent records when destroying a project

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,9 +7,9 @@ class Project < ApplicationRecord
 
   enum status: STATUSES.map { |e| [e, e.to_s] }.to_h.freeze
 
-  has_many :activities, -> { order(created_at: :desc) }
-  has_many :comments
-  has_many :status_changes
+  has_many :activities, -> { order(created_at: :desc) }, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :status_changes, dependent: :destroy
 
   validates :name, :status, presence: true
 end
