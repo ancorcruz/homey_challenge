7c44f306d2cb74153a8ce7216ad2387768cab861
Ancor Cruz <ancor.cruz.ext@qonto.com>
2023-05-09 02:01:14 +0100

    Fix specs and humanize statuses

--- spec/features/projects_create_spec.rb
+++ spec/features/projects_create_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.feature "User creates a project" do
   scenario "they see the page for the created project" do
     project_name = "Homey challenge"
-    project_status = "in_progress"
+    project_status = "In progress"
 
     visit root_path
     click_on "New project"
     fill_in "Name", with: project_name
-    fill_in "Status", with: project_status
+    select project_status, from: "Status"
     click_on "Create Project"
 
     expect(page).to have_text project_name
--- spec/features/projects_update_spec.rb
+++ spec/features/projects_update_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 RSpec.feature "User updates a project" do
   scenario "they see the page for the updated project" do
     project_name = "Homey challenge"
-    project_status = "completed"
+    project_status = "Completed"
 
     project = FactoryBot.create :project
 
     visit project_path(project)
     click_on "Edit this project"
     fill_in "Name", with: project_name
-    fill_in "Status", with: project_status
+    select project_status, from: "Status"
     click_on "Update Project"
 
     expect(page).to have_text project_name
@@ -20,17 +20,17 @@ RSpec.feature "User updates a project" do
   scenario "they see the status update history message" do
     freeze_time do
       previous_status = "in_progress"
-      new_status = "completed"
+      new_status = "Completed"
       status_changed_at = Time.zone.now.strftime "%d %B %H:%M"
 
       project = FactoryBot.create :project, status: previous_status
 
       visit project_path(project)
       click_on "Edit this project"
-      fill_in "Status", with: new_status
+      select new_status, from: "Status"
       click_on "Update Project"
 
-      expect(page).to have_text "Status changed from #{previous_status} to #{new_status} at #{status_changed_at}"
+      expect(page).to have_text "Status changed from #{previous_status.humanize} to #{new_status}\nat #{status_changed_at}"
     end
   end
 end
